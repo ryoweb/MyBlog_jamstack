057d998cb0c1967ab9ba4ecb7600b49595f7b3f7
ryo <ryotaa0917@icloud.com>
2022-11-26 11:33:23 +0900

    css

--- components/Header.js
+++ components/Header.js
@@ -3,18 +3,18 @@ import styles from '../styles/header.module.css'
 export default function Header({ children }) {
   return (
     <div className={styles.header}>
-      <div className={styles.mountain1}/>
-      <div className={styles.mountain2}/>
-      <div className={styles.blurlauer}/>
-      <div className={styles.mountain3}/>
-      <div className={styles.mountain4}/>
-      <div className={styles.mountain5}/>
-      <div className={styles.mountain6}/>
+      <div className={styles.mountain1} />
+      <div className={styles.mountain2} />
+      <div className={styles.blurlauer} />
+      <div className={styles.mountain3} />
+      <div className={styles.mountain4} />
+      <div className={styles.mountain5} />
+      <div className={styles.mountain6} />
       <h1 className={styles.title}>
         FronTeck
       </h1>
       <p className={styles.text}>
-      Just a notepad for amateur web developers
+        Just a notepad for amateur web developers
       </p>
       {children}
     </div>
